9d66aad7d9aa721105f40e4493a7957956f49ad5
arock19 <guptaa5@rose-hulman.edu>
2018-10-18 15:49:48 -0400

    o

--- src/m1_sequence_mutation.py
+++ src/m1_sequence_mutation.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  LOOPS WITHIN LOOPS, SEQUENCES and MUTATION  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Achintya Gupta.
+"""  # TO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -29,9 +29,25 @@ def run_test_zero_changer():
     print('Test 1:')
     print('  Expected:', expected1)
     print('  Actual:  ', test1)
+    # Test 2:
+    test2 = ([], [], [], [])
+    expected1 = ([], [], [], [])
+    zero_changer(test2)
+    print()
+    print('Test 2:')
+    print('  Expected:', expected1)
+    print('  Actual:  ', test2)
+    # Test 3:
+    test3 = ()
+    expected1 = ()
+    zero_changer(test3)
+    print()
+    print('Test 3:')
+    print('  Expected:', expected1)
+    print('  Actual:  ', test3)
 
     # ------------------------------------------------------------------
-    # TODO: 2. Write at least 2 additional tests for the  zero_changer   function.
+    # TO: 2. Write at least 2 additional tests for the  zero_changer   function.
     #   Try do do some unexpected things like empty lists or an empty tuple.
     # ------------------------------------------------------------------
 
@@ -60,8 +76,18 @@ def zero_changer(tuple_of_lists):
     Type hints:
       :type tuple_of_lists: tuple of list[int]
     """
+    zeronum=0
+    for j in range(len(tuple_of_lists)):
+        for k in range(len(tuple_of_lists[j])):
+            if tuple_of_lists[j][k]==0:
+                zeronum=zeronum+1
+                tuple_of_lists[j][k]=zeronum
+
+
+
+
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # TO: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
     # ------------------------------------------------------------------
